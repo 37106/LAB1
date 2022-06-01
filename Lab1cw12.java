5d0ad46a1d69f1d857c87b22bf589c672603f063
37106 <100993594+37106@users.noreply.github.com>
2022-06-01 13:10:12 +0200

    Add files via upload

---
+++ Lab1cw12.java
@@ -0,0 +1,16 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+public class Lab1cw12 {
+
+    public static void main(String[] args) {
+
+        /* Utworzenie własnej klasy         */
+
+        Account acc = new Account();
+        acc.setName("Piotr Gołąbek");
+        System.out.println(acc.getName());
+
+    }
+}
